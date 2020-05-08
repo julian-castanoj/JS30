2efeafa833a3e25b2bf60d610e19307ce223923f
julian-castanoj <50122146+julian-castanoj@users.noreply.github.com>
2020-05-07 23:07:51 -0500

    Navegación

---
+++ 24-30/javaS.js
@@ -0,0 +1,30 @@
+const nav = document.querySelector('#main');
+let topOfNav = nav.offsetTop;
+
+function fixNav() {
+  if (window.scrollY >= topOfNav) {
+    document.body.style.paddingTop = nav.offsetHeight + 'px';
+    document.body.classList.add('fixed-nav');
+  } else {
+    document.body.classList.remove('fixed-nav');
+    document.body.style.paddingTop = 0;
+  }
+}
+
+window.addEventListener('scroll', fixNav);
+
+function debounce(func, wait = 20, immediate = true) {
+    var timeout;
+    return function() {
+    var context = this, args = arguments;
+    var later = function() {
+        timeout = null;
+        if (!immediate) func.apply(context, args);
+    };
+    var callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+    };
+}
+
